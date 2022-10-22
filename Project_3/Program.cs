deb53b9ea4d35030475e57aed4d8280535e2d09c
Vladislav1 <vlad-pugovkin2@yandex.ru>
2022-10-22 11:43:55 +0300

    Add Project_3

---
+++ Project_3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int GetNumber(string message)
+{
+    int result;
+    Console.WriteLine(message);
+    while(true)
+    {
+        if(int.TryParse(Console.ReadLine(), out int number) && number > 0)
+        {
+            return number;
+        }
+        else
+        Console.WriteLine("Вы ввели не положительное число число");
+    }
+    return result;
+}
+void Cube(int n)
+{
+    for(int i = 1; i <= n; i++)
+    {
+        if(i!=n)
+        Console.Write($"{i*i*i}, ");
+        else 
+        Console.Write($"{i*i*i} ");
+    }
+}
+
+int number = GetNumber("Введите положительное число:");
+Cube(number);
